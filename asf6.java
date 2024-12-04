4e97441c74131fc6bea9eaca7749aa109ac23077
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-04 10:55:41 +0530

    Add files via upload

---
+++ asf6.java
@@ -0,0 +1,29 @@
+
+public class asf6
+{
+    public static void main(String args[])
+    {
+        
+        
+        int a = 10;
+        int pos=1;
+        
+
+        if ((a & (1<<pos))!=0)
+        {
+            System.out.println("setbit");
+
+        }
+        else
+        {
+            System.out.println("not setbit");
+        }
+
+        
+
+        
+
+
+        
+    }
+}
