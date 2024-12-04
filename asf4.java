c6a05c600a63f4101eda7bac9bf7bf965d1e4f85
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-04 10:15:00 +0530

    Add files via upload

---
+++ asf4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class asf4
+{
+    public static void main(String args[])
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter a number :");
+        int num = sc.nextInt();
+
+        if ((num&1)==0)
+
+        {
+            System.out.println("even");
+
+        }
+        else{
+            System.out.println("odd");
+        }
+
+        
+
+        
+
+
+        
+    }
+}
