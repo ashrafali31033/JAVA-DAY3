0fc053fcdbc03dd2f347233a16c6c3b26d95670a
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-04 10:27:40 +0530

    Add files via upload

---
+++ asf5.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class asf5
+{
+    public static void main(String args[])
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        //System.out.println("enter a number :");
+        int a = 5;
+        int pos =2;
+
+        if ((a&pos)==1)
+
+        {
+            System.out.println("setbit");
+
+        }
+        else{
+            System.out.println("not a  setbit");
+        }
+
+        
+
+        
+
+
+        
+    }
+}
